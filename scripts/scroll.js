eb40dcba697c3cb4205867bb42da4a07600f80b9
Somov Eugene <somov.eugene.2012@gmail.com>
2022-01-22 19:28:45 +0300

    Encapsulated module code

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,22 +1,26 @@
-const linksMenu = document.querySelectorAll(".header-menu__item a");
-const linkCharacteristics = document.querySelector(".card-details__link-characteristics");
-const allLinks = [...linksMenu, linkCharacteristics];
+const smoothScroll = () => {
+  const linksMenu = document.querySelectorAll(".header-menu__item a");
+  const linkCharacteristics = document.querySelector(".card-details__link-characteristics");
+  const allLinks = [...linksMenu, linkCharacteristics];
 
-// patch all methods polyfill
-seamless.polyfill();
+  // patch all methods polyfill
+  seamless.polyfill();
 
-allLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const id = link.getAttribute("href").substring(1);
-    const section = document.getElementById(id);
+  allLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const id = link.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
 
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-        inline: "start",
-      })
-    }
-  })
-});
+      if (section) {
+        seamless.elementScrollIntoView(section, {
+          behavior: "smooth",
+          block: "start",
+          inline: "start",
+        });
+      }
+    });
+  });
+};
+
+smoothScroll();
